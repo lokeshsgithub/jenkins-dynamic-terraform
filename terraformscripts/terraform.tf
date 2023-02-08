3f54baf588e8bc514314718f0db293985844f93e
Lokesh <lokeshreddy05690@gmail.com>
2023-02-08 13:15:57 +0530

    Update terraform.tf

--- terraformscripts/terraform.tf
+++ terraformscripts/terraform.tf
@@ -3,7 +3,7 @@
 }
 
 resource "aws_instance" "AWSINSTANCE" {
-  ami = "ami-0f9d9a251c1a44858"
+  ami= "ami-0f9d9a251c1a44858"
   instance_type = "t2.micro"
   key_name = "lokeshsoftwaresolutions"
   security_groups = ["launch-wizard-7"]
