d1e19ca42e565cc1565bd5ad1a86e76fdf1804ce
Lokesh <lokeshreddy05690@gmail.com>
2023-02-08 12:07:17 +0530

    Create terraform.tf

---
+++ terraform.tf
@@ -0,0 +1,14 @@
+ provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "LOKINSTANCE" {
+  ami= "ami-0f9d9a251c1a44858"
+  instance_type = "t2.micro"
+  security_groups = ["launch-wizard-7"]
+  key_name = "lokeshsoftwaresolutions"
+  tags = {
+  Name = "lokeshtomcat"
+  }
+}
+ 
